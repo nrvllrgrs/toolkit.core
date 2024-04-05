83ca0e88f1e917fec5e9d1f67d57449f51a68559
TITAN\ckilg <ckilgore@gmail.com>
2024-04-05 07:50:56 -0500

    Added missing abstract class for SpawnerEvent units

---
+++ Samples~/Visual Scripting/Units/Spawning/BaseSpawnerEventUnit.cs
@@ -0,0 +1,8 @@
+using Unity.VisualScripting;
+
+namespace ToolkitEngine.VisualScripting
+{
+	[UnitCategory("Events/Spawning")]
+	public abstract class BaseSpawnerEventUnit : BaseEventUnit<SpawnerEventArgs>
+	{ }
+}
