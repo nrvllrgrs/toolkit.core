3bfbab138869da2c1d1dcaa8d997220acb8122a9
TITAN\ckilg <ckilgore@gmail.com>
2024-10-11 09:13:14 -0500

    Added horizontal only check

--- Runtime/Scoring/Evaluators/AngleEvaluator.cs
+++ Runtime/Scoring/Evaluators/AngleEvaluator.cs
@@ -9,6 +9,9 @@ namespace ToolkitEngine
 		[SerializeField, MinMax(0f, 180f), Tooltip("Minimum and maximum angles (degrees) relative to gameObject's forward vector. Value less than minimum returns 0; value greater than maximum returns 1.")]
 		private Vector2 m_range = new Vector2(0f, 60f);
 
+		[SerializeField, Tooltip("Indicates whether angle should only be evaluated on X-Z plane.")]
+		private bool m_horizontalOnly = false;
+
 		#endregion
 
 		#region Properties
@@ -22,7 +25,19 @@ namespace ToolkitEngine
 
 		protected override float CalculateNormalizedScore(GameObject actor, GameObject target, Vector3 position)
 		{
-			float angle = Vector3.Angle(actor.transform.forward, position - actor.transform.position);
+			float angle;
+			if (!m_horizontalOnly)
+			{
+				angle = Vector3.Angle(actor.transform.forward, position - actor.transform.position);
+			}
+			else
+			{
+				var forward2D = new Vector2(actor.transform.forward.x, actor.transform.forward.z);
+				var position2D = new Vector2(position.x, position.z);
+				var actorPosition2D = new Vector2(actor.transform.position.x, actor.transform.position.z);
+				angle = Vector2.Angle(forward2D, position2D - actorPosition2D);
+			}
+
 			return MathUtil.GetPercent(angle, minAngle, maxAngle);
 		}
 
--- Runtime/Scoring/Evaluators/DistanceEvaluator.cs
+++ Runtime/Scoring/Evaluators/DistanceEvaluator.cs
@@ -9,6 +9,9 @@ namespace ToolkitEngine
 		[SerializeField, MinMax(0f, float.PositiveInfinity)]
 		private Vector2 m_range = new Vector2(0f, 100f);
 
+		[SerializeField, Tooltip("Indicates whether angle should only be evaluated on X-Z plane.")]
+		private bool m_horizontalOnly = false;
+
 		#endregion
 
 		#region Properties
@@ -22,10 +25,19 @@ namespace ToolkitEngine
 
 		protected override float CalculateNormalizedScore(GameObject actor, GameObject target, Vector3 position)
 		{
-			return MathUtil.GetPercent(
-				(actor.transform.position - position).sqrMagnitude,
-				minDistance * minDistance,
-				maxDistance * maxDistance);
+			float sqrDistance;
+			if (!m_horizontalOnly)
+			{
+				sqrDistance = (actor.transform.position - position).sqrMagnitude;
+			}
+			else
+			{
+				var position2D = new Vector2(position.x, position.z);
+				var actorPosition2D = new Vector2(actor.transform.position.x, actor.transform.position.z);
+				sqrDistance = (actorPosition2D - position2D).sqrMagnitude;
+			}
+
+			return MathUtil.GetPercent(sqrDistance, minDistance * minDistance, maxDistance * maxDistance);
 		}
 
 		#endregion
