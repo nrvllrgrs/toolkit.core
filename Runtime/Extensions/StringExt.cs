d51f610bcc832d153193f8dd131589805a11fafe
TITAN\ckilg <ckilgore@gmail.com>
2025-01-16 11:02:18 -0600

    Added SnakeCaseToString Method

--- Runtime/Extensions/StringExt.cs
+++ Runtime/Extensions/StringExt.cs
@@ -6,4 +6,6 @@ public static class StringExt
 	public static string InsertSpacesToCamelCase(this string s) => Regex.Replace(s, @"(?<!^)(?=[A-Z])", " ");
 
 	public static string CamelCaseToTitleCase(this string s) => CultureInfo.CurrentCulture.TextInfo.ToTitleCase(InsertSpacesToCamelCase(s));
+
+	public static string SnakeCaseToString(this string s) => s.Replace('_', ' ');
 }
