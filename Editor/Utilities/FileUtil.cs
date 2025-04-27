c6fbff02512650be55bcdfebe6a807dcc718bb62
TITAN\ckilg <ckilgore@gmail.com>
2025-04-27 07:36:08 -0500

    Added getAssetAtAbsolutePath

--- Editor/Utilities/FileUtil.cs
+++ Editor/Utilities/FileUtil.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using UnityEditor;
+using UnityEditor.VersionControl;
 using UnityEngine;
 
 namespace ToolkitEditor
@@ -8,7 +9,31 @@ namespace ToolkitEditor
     {
         public static string GetAbsolutePath(Object asset)
         {
+            if (asset == null)
+                return string.Empty;
+
             return Path.Combine(Application.dataPath, AssetDatabase.GetAssetPath(asset).Substring("/Assets".Length));
+		}
+
+        public static string GetRelativePath(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+                return null;
+
+            if (!path.StartsWith(Application.dataPath, System.StringComparison.OrdinalIgnoreCase))
+                return path;
+
+            return "Assets" + path.Substring(Application.dataPath.Length);
+        }
+
+        public static T GetAssetAtAbsolutePath<T>(string path)
+            where T : Object
+        {
+            string relativePath = GetRelativePath(path);
+            if (relativePath == null)
+                return null;
+
+            return AssetDatabase.LoadAssetAtPath<T>(relativePath);
 		}
 
         public static void Move(string srcPath, string dstPath, bool overridePath = true)
